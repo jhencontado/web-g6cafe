ff86ee7c71d76d60547c60a5b92d4569ad5d854d
Chie <sheeboom24@gmail.com>
2025-01-21 15:45:29 +0800

    checkout new

--- g6cafeCX/app.py
+++ g6cafeCX/app.py
@@ -60,7 +60,7 @@ class PwdSeniorDetails(db.Model):
     __tablename__ = 'pwdsenior_details'
     pwdsenior_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id', ondelete='CASCADE'))
-    discount_type = db.Column(db.String(50), nullable=False)
+    discount_type = db.Column(db.String(50))
     customer_name = db.Column(db.String(100), nullable=False)
     id_number = db.Column(db.String(100), nullable=False)
     discount_amount = db.Column(db.Numeric(10, 2), nullable=False)
